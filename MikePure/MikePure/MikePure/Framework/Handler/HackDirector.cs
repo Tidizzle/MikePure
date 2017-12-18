5c8cdf3cec96c83920627fc5f73f16c16baaa8eb
Zach Taylor <zach@planetjester.com>
2017-12-17 22:35:52 -0500

    added todos

--- MikePure/MikePure/MikePure/Framework/Handler/HackDirector.cs
+++ MikePure/MikePure/MikePure/Framework/Handler/HackDirector.cs
@@ -2,6 +2,90 @@
 {
     public class HackDirector
     {
+        #region TODO
+
+        //TODO: Implement Hook
+        //TODO: Generate AP call loop
+        //TODO: Generate HD methods
+        //TODO: Add GO Manipulation to HD
+        //TODO: Implement local loading in LFH
+        //TODO: Add methods for editing local files during runtime
+        //TODO: populate coloroption enum
+        //TODO: populate nvoption enum
+        //TODO: add members to friend
+        //TODO: Add members to Gunasset based on tsunami 
+        //TODO: Add members to keybind
+        //TODO: Populate submenus with feature specific variables
+        //TODO: Add sub methods for start, update, etc in partial classes
+        //TODO: flesh menuhandler with universal items
+        //TODO: Add menucontroller to menuhandler
+        //TODO: Populate logging based on tsunami
+        //TODO: Populate PlayerManipulation with related methods 
+
+            #region Aim
+                //TODO: Flesh menu
+                //TODO: Add Aimbot
+                    //TODO: Add FOV Circle
+                    //TODO: Add distance to player, distance to xhair, danger settings
+                //TODO: Add Aimlock
+                //TODO: Add Triggerbot
+                
+            #endregion
+        
+            #region Visuals
+                //TODO: Flesh Menu
+                //TODO: Add 2d box
+                //TODO: Add 3d Box
+                //TODO: Add Skeleton
+                //TODO: Add Snaplines (Tracers)
+                //TODO: Add Glow (Just add this for main shit to save headache
+                //TODO: Add Labels
+                    //TODO: Add player Health, Name, Distance, Weapon, Etc
+                    //TODO: Add zombie (ditto)
+                    //TODO: Add Storages
+                    //TODO: Add Vehicles
+                    //TODO: Add Items (Make sure to interface correctly with itemselection
+                    //TODO: Add Animals
+                    //TODO: Add Admin Overhead warning
+                    //TODO: Add scaling
+                //TODO: Add admin chat warning
+                //TODO: Add Environment
+                //TODO: Add Getting high
+                //TODO: Add NV
+                //TODO: Add Set time
+                //TODO: Add Set zoom / zoom without scope
+                //TODO: Add Color Interfacing
+                
+
+            #endregion
+        
+            #region ItemSelection
+                //TODO: Find way to get actual item id list for item selection
+                //TODO: Flesh Menu
+                //TODO: Interace with item menu
+
+            #endregion
+        
+            #region Keybinds
+                //TODO: Flesh Menu
+                //TODO: Add Dynamic list
+                //TODO: Add Changing
+                //TODO: Add MenuToggle, Aimbottoggle, panic toggle, esptoggle, others
+        
+            #endregion
+
+            #region Player
+                //TODO: Flesh menu
+                    //TODO: Add Friends system (This is a royal pain in the ass btw)
+                //TODO: Add gun mods
+                //TODO: Add Camera freeflight
+                //TODO: Add Advanced Rangefinder / Player Info (Find better name for this)
+                //TODO: Add Vehicle fly
+
+            #endregion
+        
+        #endregion
+
         
     }
 }
